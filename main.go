18003a546f6df5d1530f6df75853c85913ada750
Viky <vigneshvics13@gmail.com>
2023-04-20 16:51:19 +0530

    access control origin

--- main.go
+++ main.go
@@ -36,6 +36,13 @@ func main() {
 	DatabaseConnection()
 	//server up process
 	r:= mux.NewRouter()
+	r.Use(func(next http.Handler) http.Handler {
+        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+            w.Header().Set("Access-Control-Allow-Origin", "*")
+            next.ServeHTTP(w, r)
+        })
+    })
+
 	fmt.Println("starting server")
 	r.HandleFunc("/movie/explore", ExploreMovies).Methods("GET")
 	r.HandleFunc("/",welcome).Methods("GET")
