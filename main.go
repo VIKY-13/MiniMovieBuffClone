bc6be3e99cebe5ad80dcd1b2e2e3045a1b265eb4
Viky <vigneshvics13@gmail.com>
2023-05-11 22:12:36 +0530

    handled get request for fav & watchlist

--- main.go
+++ main.go
@@ -49,11 +49,11 @@ func main() {
 	//server up process
 	r:= mux.NewRouter()
 	r.Use(func(next http.Handler) http.Handler {
-        return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-            w.Header().Set("Access-Control-Allow-Origin", "*")
-            next.ServeHTTP(w, r)
-        })
-    })
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			next.ServeHTTP(w, r)
+		})
+	})
 	fmt.Println("starting server")
 	r.HandleFunc("/movie/explore", controllers.ExploreMovies).Methods("GET")
 	r.HandleFunc("/",welcome).Methods("GET")
